1608a86ead13ca77b714ab7600806abba8c7d50f
Angel Quintanilla <55508446+angelusm2003@users.noreply.github.com>
2023-05-24 23:32:02 -0600

    Add files via upload

---
+++ vertec/src/test/java/com/words/star/TestTextAnalysisService.java
@@ -0,0 +1,22 @@
+package com.words.star;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class TestTextAnalysisService {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new TextAnalysisService(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+}
